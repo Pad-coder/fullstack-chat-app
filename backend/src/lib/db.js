68f979b56b016e17e01ed7a2ca7fbfc17c946e33
Padmanaban <padmanaban870@gmail.com>
2025-03-28 00:24:58 +0530

    Add package-lock.json and update dependencies in package.json files

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect("mongodb+srv://padmanaban870:NAAGt2BWr9WkIxDg@cluster0.r8nncjg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
